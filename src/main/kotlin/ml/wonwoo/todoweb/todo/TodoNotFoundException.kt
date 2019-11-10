2ed12a285611285d0fdbbd70145070e4d229fa8a
wonwoo <aoruqjfu@gmail.com>
2019-11-10 21:11:26 +0900

    spring boot version upgrade

--- src/main/kotlin/ml/wonwoo/todoweb/todo/TodoNotFoundException.kt
+++ src/main/kotlin/ml/wonwoo/todoweb/todo/TodoNotFoundException.kt
@@ -3,7 +3,6 @@ package ml.wonwoo.todoweb.todo
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
 
-
 @ResponseStatus(HttpStatus.NOT_FOUND)
 class TodoNotFoundException(override val message: String)
     : RuntimeException()
